60f421618524e7b068d5bfd51987860a8cacd0a4
Josh Bullock <mail@joshb.me>
2022-09-23 10:21:21 -0400

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to josh-clojure-discovery
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/josh_clojure_discovery/core_test.clj
@@ -0,0 +1,7 @@
+(ns josh-clojure-discovery.core-test
+  (:require [clojure.test :refer :all]
+            [josh-clojure-discovery.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
