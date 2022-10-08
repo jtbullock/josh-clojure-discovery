c74720dfb2b3f24af53a3f233a213f2beb28fc28
Josh Bullock <mail@joshb.me>
2022-10-07 21:31:08 -0400

    Generate the HTML!

--- src/josh_clojure_discovery/clojure-workshop/chapter-6/html-generator.clj
+++ src/josh_clojure_discovery/clojure-workshop/chapter-6/html-generator.clj
@@ -11,31 +11,34 @@
      [:p "But Clojure uses vectors instead."]]
     [:div
      [:p "Converting nested lists into HTML is an old Lisp trick"]
-     [:p "But Clojure uses vectors instead."]]
-    ]])
-
+     [:p "But Clojure uses vectors instead."]]]])
+    
 (def simple-example [:html [:p "Para 1"] [:p "Para 2"]])
 
 (defn write-attributes
   "Given a map of `attributes`, return a string of the attributes
    in HTML form." 
   [attributes]
-  (map (fn [[attr val]] (str (name attr) "=\"" val "\"")) attributes)
-)
+  (apply str (map (fn [[attr val]] (str " " (name attr) "=\"" val "\"")) attributes)))
 
-(defn convert-to-html
-  "Given a vector of HTML tags, return a string as HTML"
-  [html-vector]
-  (let [current-tag (first html-vector)
-        ;current-tag-str (name current-tag)
-        tag-content (subvec html-vector 1)
-        child-content (map (fn [thing] (cond
-                                         (map? thing)
-                                         "attr='attr'"
-                                         (string? thing)
-                                         thing
-                                         (vector? thing)
-                                         (convert-to-html thing))) tag-content)
-        ]
-    (str "<" (name current-tag) ">" (apply str child-content) "</" (name current-tag) ">"))
-)
+(defn parse-tag-vector [tag-vector]
+  (let [tag (first tag-vector)]
+    (reduce
+     (fn [acc, item] 
+      (cond 
+        (map? item) (assoc acc :attributes item)
+        (string? item) (assoc acc :content item)
+        (vector? item) (update acc :children conj (parse-tag-vector item))))
+     {:tag tag :attributes {} :content "" :children []}
+     (rest tag-vector))))
+
+(defn print-html*
+  [parsed-vec]
+  (let [tag-str (name (parsed-vec :tag))
+        formatted-attributes (write-attributes (parsed-vec :attributes))
+        printed-children (map (fn [x] (print-html* x)) (parsed-vec :children))
+        children-str (apply str printed-children)
+        content (parsed-vec :content)]
+    (str "<" tag-str formatted-attributes ">" content children-str "</" tag-str ">")))
+
+(defn print-html [vec-html] (print-html* (parse-tag-vector vec-html)))
