368bb5b760bc497732b48f37fc1c71a7eda17d28
Josh Bullock <mail@joshb.me>
2022-10-11 11:52:48 -0400

    Chapter 7

--- src/josh_clojure_discovery/clojure-workshop/chapter-7-scratchpad.clj
+++ src/josh_clojure_discovery/clojure-workshop/chapter-7-scratchpad.clj
@@ -1,5 +1,8 @@
 (ns chapter-7-scratchpad)
 
+; Lazy sequences
+
+; Exercise 7.01
 (def sample-data
   [[24.2 420031]
    [25.8 492657]
@@ -10,5 +13,65 @@
    [23.1 836204]                        ;min
    [23.5 884120]])
    
+(defn local-max? [[[a _] [b _] [c _]]]
+  (and (< a b)
+       (< c b)))
 
+(defn local-min? [[[a _] [b _] [c _]]]
+  (and (> a b)
+       (> c b)))
 
+(defn inflection-points [data]
+  (lazy-seq
+   (let [current-series (take 3 data)]
+     (cond
+       (< (count current-series) 3) '()
+       (local-max? current-series)
+        (cons
+          (conj (second current-series) :peak)
+          (inflection-points (rest data)))
+       (local-min? current-series)
+        (cons
+          (conj (second current-series) :valley)
+          (inflection-points (rest data)))
+       :otherwise (inflection-points (rest data))))))
+
+; Exercise 7.02
+(def endless-potatoes (repeatedly (fn [] (+ 10 (rand-int 390)))))
+
+(defn average-potatoes [prev arrivals]
+  (lazy-seq 
+   (if-not arrivals 
+     '()
+     (let [[_ n total] prev
+           current [(first arrivals) (inc (or n 0)) (+ (first arrivals) (or total 0))]]
+       (cons current (average-potatoes current (next arrivals)))
+     )
+   )))
+
+; Exercise 7.03
+
+; My activity: family tree
+(def family-parents [
+  ["Josh Bullock" ["Judith Jenkins" "John Bullock"]]
+  ["Judith Jenkins" ["Regina Rauckhorst" "Albert Jenkins"]]
+  ["John Bullock" ["Ruth Balog" "Gary Bullock"]]
+  ["Regina Rauckhorst" ["Great Grandma 1" "Great Grandpa 1"]]
+  ["Albert Jenkins" ["Great Grandma 2" "Great Grandpa 2"]]
+  ["Ruth Balog" ["Great Grandma 3" "Great Grandpa 3"]]
+  ["Gary Bullock" ["Great Grandma 4" "Great Grandpa 4"]]
+])
+
+(def birth-parent-map (into {} family-parents))
+
+(defn build-tree
+  [name birth-parent-lookup]
+  (lazy-seq
+  (let [parents (get birth-parent-lookup name)]
+    (if (nil? parents) 
+      '()
+      [name [(build-tree (first parents) birth-parent-lookup) (build-tree (second parents) birth-parent-lookup)]]
+    )
+  )
+  )
+)
---
+++ src/josh_clojure_discovery/clojure-workshop/chapter-7/tennis-elo-tree.clj
@@ -0,0 +1,36 @@
+(ns tennis-elo-tree
+  (:require
+    [clojure.math.numeric-tower :as math]
+    [clojure.java.io :as io]
+    [clojure.data.csv :as csv]
+    [semantic-csv.core :as sc]))
+
+(defn match-probability [player-1-rating player-2-rating]
+  (/ 1
+     (+ 1
+        (math/expt 10 (/ (- player-2-rating player-1-rating)
+                         400)))))
+
+(defn recalculate-rating [previous-rating expected-outcome real-outcome k-factor]
+  (+ previous-rating (* k-factor (- real-outcome expected-outcome))))
+
+(defn elo-world-simple
+  [csv-path k-factor]
+  (with-open [r (io/reader csv-path)]
+    (->> (csv/read-csv r)
+         sc/mappify
+      ;(sc/cast-with {:winner_sets_won sc/->int :loser_sets_won sc/->int})
+         (reduce (fn [acc {:keys [winner_slug loser_slug]}]
+                   (let [winner_rating (get-in acc [:player_ratings winner_slug] 400)
+                         loser_rating (get-in acc [:player_ratings loser_slug] 400)
+                         winner_probability (match-probability winner_rating loser_rating)
+                         loser_probability (match-probability loser_rating winner_rating)
+                         able_to_predict? (not (= winner_probability loser_probability))
+                         predition_correct? (> winner_probability loser_probability)]
+                     (-> acc
+                         (assoc-in [:player_ratings winner_slug] (recalculate-rating winner_rating winner_probability 1 k-factor))
+                         (assoc-in [:player_ratings loser_slug] (recalculate-rating loser_rating loser_probability 0 k-factor))
+                         (update :total_matches inc)
+                         (update :prediction_count #(if able_to_predict? (inc %) %))
+                         (update :success_count #(if predition_correct? (inc %) %)))))
+                 {:player_ratings {}, :success_count 0, :matches [], :prediction_count 0}))))
